ac9f954ce5df469173aa8818ed15dc81dd831ea7
suryamurugan <surya3542live@gmail.com>
2018-05-02 16:25:56 +0530

    Initial commit

---
+++ app/src/main/java/app/mindweaversolutions/trucksta/trucksta/MainActivity.java
@@ -0,0 +1,13 @@
+package app.mindweaversolutions.trucksta.trucksta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
